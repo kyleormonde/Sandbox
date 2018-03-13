68a3e6bdc9aa2f0a33629550b504afd5f127c2b8
Kyle Ormonde <kyle.ormonde@my.jcu.edu.au>
2018-03-13 14:42:15 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 """Kyle Alexander Ormonde"""
+name = str(input("Name: "))
+while not name:
+    print("Name cannot be blank.")
+    name = str(input("Name: "))
+for i in range(0, len(name), 2):
+    print(name[i])
